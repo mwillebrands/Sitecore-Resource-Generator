8a9c76fbeb0651b0d7f9a500e1200a422004c7d3
Maarten Willebrands <maarten.willebrands@creates.nl>
2021-05-22 13:54:28 +0200

    Initial commit

---
+++ SitecoreResourceGenerator/CommandLineOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace SitecoreResourceGenerator
+{
+    public class CommandLineOptions
+    {
+        [Option('f', "file", Required = true, HelpText = "The path to the .scitempackage")]
+        public string FilePath { get; set; }
+
+        [Option('d', "database", Required = true, HelpText = "The database for the items")]
+        public string Database { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "The path for the output file, the extension is automatically added")]
+        public string OutputPath { get; set; }
+    }
+}
---
+++ SitecoreResourceGenerator/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using CommandLine;
+using Microsoft.Extensions.Logging.Abstractions;
+using ProtoBuf;
+using Sitecore.Data.DataProviders.ReadOnly.Protobuf.Data;
+using Sitecore.DevEx.Configuration;
+using Sitecore.DevEx.Serialization.Client.Datasources.Filesystem.Configuration;
+using Sitecore.DevEx.Serialization.Client.Datasources.Filesystem.Formatting.Yaml;
+using Sitecore.DevEx.Serialization.Client.Datasources.Package;
+
+namespace SitecoreResourceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed<CommandLineOptions>(o =>
+                {
+                    if (!File.Exists(o.FilePath))
+                    {
+                        Console.Error.Write($"The package could not be found ({o.FilePath})");
+                    }
+
+                    GenerateProtobuf(o.FilePath, o.Database, o.OutputPath);
+                });
+        }
+
+        static void GenerateProtobuf(string inputPath, string database, string outputPath)
+        {
+            ItemPackage package = new ItemPackage(new FileInfo(inputPath),
+                PackageMode.Install,
+                new FilesystemConfigurationManager(new NullLoggerFactory()),
+                new FilesystemSerializationModuleConfigurationManager(new NullLoggerFactory()),
+                new YamlSerializationFormatter(),
+                new NullLoggerFactory());
+            package.ReadModuleConfigurations();
+
+            var serializationContainer = new ItemsData();
+            foreach (var itemMetaData in package.ReadItemMetadatas(database).Result)
+            {
+                var item = package.ReadItemData(database, itemMetaData.Path).Result;
+                serializationContainer.Definitions.Add(item.Id, new ItemRecord()
+                {
+                    ID = item.Id,
+                    MasterID = item.BranchId,
+                    Name = item.Name,
+                    ParentID = item.ParentId,
+                    TemplateID = item.TemplateId
+                });
+
+                if (item.SharedFields.Any())
+                {
+                    serializationContainer.SharedData.Add(item.Id,
+                        item.SharedFields.ToDictionary(x => x.FieldId, x => x.Value));
+                }
+
+                var languageData = new ItemLanguagesData();
+                foreach (var languageVersions in item.Versions.GroupBy(x => x.Language))
+                {
+                    var versionData = new VersionsData();
+                    foreach (var version in languageVersions)
+                    {
+                        var fieldsData = new FieldsData();
+                        foreach (var versionFields in version.Fields)
+                        {
+                            fieldsData.Add(versionFields.FieldId, versionFields.Value);
+                        }
+                        versionData.Add(version.VersionNumber, fieldsData);
+                    }
+                    languageData.Add(languageVersions.Key, versionData);
+                }
+
+                foreach (var languageVersion in item.UnversionedFields)
+                {
+                    var fieldsData = new FieldsData();
+                    foreach (var versionFields in languageVersion.Fields)
+                    {
+                        fieldsData.Add(versionFields.FieldId, versionFields.Value);
+                    }
+
+                    if (!languageData.ContainsKey(languageVersion.Language))
+                    {
+                        var versionData = new VersionsData { { 0, fieldsData } };
+                        languageData.Add(languageVersion.Language, versionData);
+                    }
+                    else
+                    {
+                        languageData[languageVersion.Language].Add(0, fieldsData);
+                    }
+                }
+                serializationContainer.LanguageData.Add(item.Id, languageData);
+            }
+
+
+            var outputFilePath = $"{outputPath}.dat";
+            using (var file = File.OpenWrite(outputFilePath))
+            {
+                Serializer.Serialize(file, serializationContainer);
+            }
+            Console.WriteLine($"All done, the resource file can be found at: {outputFilePath}");
+        }
+    }
+}
